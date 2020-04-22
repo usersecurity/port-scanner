8c199829267c6f281c75da0f741934f52fd8aa57
Daniel Aparecido <daniel@positivo.www.roteador.multilaser.com>
2020-04-22 12:23:36 -0300

    initial comit

---
+++ func.py
@@ -0,0 +1,97 @@
+from socket import *
+import threading
+import sys
+import time
+
+def getArgs(args, usage):
+	'''
+	pega os argumentos passados na linha de comando
+	e retorna uma lista com as opcoes fornecidas
+	'''
+
+	host = args[len(args) - 1]
+
+	#valores padroes
+	#-----------------------------------
+
+	n = 0
+	timeout = 2
+	ports = False
+
+	#-----------------------------------
+
+	while n < len(args):
+		if args[n] == "-p":
+			try:
+				ports = getPorts(args[n + 1])
+				if ports == False:
+					print(usage)
+					exit(0)
+			except:
+				print(usage)
+				exit(0)
+
+		elif args[n] == "-h":
+				print(usage)
+				exit(0)
+		elif args[n] == "-v":
+			v = True
+
+		elif args[n] == "-t":
+			timeout = int(args[n + 1])
+		n += 1
+
+	if ports == False:
+		print(usage)
+		exit(0)
+
+	return [ports, v, timeout]
+
+def getPorts(range_ports):
+
+	'''
+	---------------------------------------------------------------
+	retorna as portas a serem escaneadas
+	caso o usuario nao forneça uma porta ou range de portas valido,
+	retorna false
+	---------------------------------------------------------------
+	'''
+
+	ports = []
+
+	if "-" in range_ports:
+		p = range_ports.split("-")
+
+		if len(p) == 2:
+			nport, final_port = [int(p[0]), int(p[1])]
+
+			while nport <= final_port: #adiciona as portas na lista ports
+				ports.append(nport)
+				nport += 1
+			return ports
+
+		else:
+			return False
+
+	else:
+		''' caso seja informada apenas uma porta, ela é adicionada a lista '''
+		try:
+			ports.append(int(range_ports))
+			return ports
+		except:
+			return False;
+
+def scanner(host, port, v, timeout, open_ports, closed_ports):
+
+	s = socket(AF_INET, SOCK_STREAM)
+	s.settimeout(timeout)
+
+	addr = (host, port)
+
+	if s.connect_ex(addr) == False:
+		open_ports.append(port)
+
+	else:
+		closed_ports.append(port)
+
+	s.close()
---
+++ scanner.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from func import *
+import sys
+
+def main(args):
+	usage = "Usage: " + args[0] + " -p [ports] host\n" + "options:\n" + "\t-h\t\tshow this help" + "\n\t-p [ports]\tports to scan\n"
+
+	host = args[len(args) - 1]
+
+	ports, v, timeout = getArgs(args, usage)
+
+	threads = []
+	open_ports = []
+	closed_ports = []
+
+	print("Starting scan at: %s" % time.ctime())
+
+	for port in ports:
+		t = threading.Thread(target=scanner, args=(host, port, v, timeout, open_ports, closed_ports))
+		threads.append(t)
+
+	for i in range((ports[len(ports) - 1] - ports[0])):
+		threads[i].start()
+
+	for port in open_ports:
+		print("port %s is open" % str(port))
+
+	if v:
+		for port in closed_ports:
+			print("port %s is closed" % str(port))
+
+	print("Scan finished at: %s" % time.ctime())
+
+if __name__ == "__main__":
+	main(sys.argv)
